342929b6a064c7a87bc71e7cfb5efd1c5ca9ce56
kepocnhh <kepocnhh@gmail.com>
2016-05-30 23:55:36 +0300

    init project with core

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BlockNoteDesktop
 BlockNote desktop app JavaFX
+
+# Windows build information
+## build
+> "%JAVA_HOME%/bin/javac" -sourcepath ./src/main/java -d bin -classpath "%JAVA_HOME%/jre/lib/jfxrt.jar" ./src/main/java/stan/block/note/Main.java
+
+## run
+> "%JAVA_HOME%/bin/java" -classpath "%JAVA_HOME%/jre/lib/jfxrt.jar";bin stan.block.note.Main
---
+++ src/main/java/stan/block/note/Main.java
@@ -0,0 +1,31 @@
+package stan.block.note;
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.Modality;
+
+import stan.block.note.ui.panes.BNPane;
+
+public class Main
+    extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setAlwaysOnTop(true);
+        Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+        primaryStage.setScene(new Scene(new BNPane(), screen.getWidth(), screen.getHeight(), Color.BLUE));
+        primaryStage.show();
+    }
+}
