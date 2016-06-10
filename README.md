f33a5e3862759c44f662a588fd6f62083f9815ac
antonoctopod <a.grechushnikov@octopod.com>
2016-06-10 19:46:39 +0300

    work with listview

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ BlockNote desktop app JavaFX
 ## build
 > "%JAVA_HOME%/bin/javac" -sourcepath ./src/main/java -d bin -classpath "%JAVA_HOME%/jre/lib/jfxrt.jar" ./src/main/java/stan/block/note/Main.java
 
+## build_css
+> "%JAVA_HOME%/bin/javapackager" -createbss -srcdir ./src/main/css -outdir bin/css -srcfiles theme.css -v
+
 ## run
 > "%JAVA_HOME%/bin/java" -classpath "%JAVA_HOME%/jre/lib/jfxrt.jar";bin stan.block.note.Main
--- src/main/java/stan/block/note/ui/panes/BNPane.java
+++ src/main/java/stan/block/note/ui/panes/BNPane.java
@@ -4,16 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Control;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 
-import javafx.geometry.Pos;
-import javafx.scene.text.TextAlignment;
+import javafx.util.Callback;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class BNPane
     extends HBox
@@ -58,9 +56,38 @@ public class BNPane
     }
     private ListView<String> initListView()
     {
-        ObservableList<String> items = FXCollections.observableArrayList("Single", "Double", "Suite", "Family App");
+		ObservableList<String> items = FXCollections.observableArrayList(
+            "chocolate", "salmon", "gold", "coral", "darkorchid",
+            "darkgoldenrod", "lightsalmon", "black", "rosybrown", "blue",
+            "blueviolet", "brown");
 		ListView<String> list = new ListView<String>();
         list.setItems(items);
+        list.setCellFactory(new Callback<ListView<String>, 
+            ListCell<String>>()
+			{
+                @Override 
+                public ListCell<String> call(ListView<String> list)
+				{
+                    return new ColorRectCell();
+                }
+            }
+        );
 		return list;
 	}
+	
+    static class ColorRectCell extends ListCell<String>
+	{
+        @Override
+        public void updateItem(String item, boolean empty)
+		{
+            super.updateItem(item, empty);
+			setId("main_list_cell");
+            if (item != null)
+			{
+				setGraphic(null);
+                setText(item);
+            }
+        }
+    }
+	
 }
