9b2dab44f6304e9deb6ea1f531dbb5548dbe8d79
kepocnhh <kepocnhh@gmail.com>
2016-06-12 13:48:29 +0300

    work with design and create ui

--- src/main/java/stan/block/note/ui/panes/BNPane.java
+++ src/main/java/stan/block/note/ui/panes/BNPane.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 import javafx.util.Callback;
@@ -14,45 +15,88 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class BNPane
-    extends HBox
+    extends VBox
 {
     //VIEWS
     Button back = new Button();
+    Button close = new Button();
     Label blockName = new Label();
+    Label tableName = new Label();
     ListView<String> listView;
 
 	public BNPane()
 	{
         super();
         this.setStyle("-fx-background-color: null");
-        this.getChildren().add(initLeftPane());
+        //this.getChildren().addAll(initTestPane());
+        this.getChildren().addAll(initTopPane(), initMainPane());
 	}
 
-    private VBox initLeftPane()
+    private HBox initTestPane()
     {
-		int boxW = 192;
-        VBox blockInfo = new VBox();
-        blockInfo.setStyle("-fx-background-color: red");
-		blockInfo.setPrefWidth(boxW);
-		listView = initListView();
-		listView.prefHeightProperty().bind(blockInfo.heightProperty());
-        blockInfo.getChildren().addAll(initLeftTopPane(), listView);
-        return blockInfo;
+	    HBox hbox = new HBox(10);
+        hbox.setStyle("-fx-background-color: green");
+	    Label field = new Label();
+	    field.setMaxWidth(Double.MAX_VALUE);
+	    HBox.setHgrow(field, Priority.ALWAYS);
+	    hbox.getChildren().addAll(
+	        new Label("Search:"), field, new Button("Go")
+	    );
+	    return hbox;
     }
-    private HBox initLeftTopPane()
+    private HBox initTopPane()
     {
 		int boxH = 36;
         HBox blockTop = new HBox();
 		blockTop.setMinHeight(boxH);
         blockTop.setStyle("-fx-background-color: green");
+        //
         back.setId("back_button");
 		back.setMinWidth(boxH);
 		back.prefHeightProperty().bind(blockTop.heightProperty());
-        blockName.setText("block nameaaaaaaaaaaaaaaaaaaaaaa!!!!---+aaaaaaaaaaaaaaaaaaaaaaa!");
+		//
+        blockName.setText("blockName nameaaaaaaaaaaaaaaaaaaaaaa!!!!---+!");
         blockName.setStyle("-fx-background-color: orange");
 		blockName.prefHeightProperty().bind(blockTop.heightProperty());
-        blockTop.getChildren().addAll(back, blockName);
+		//blockName.prefWidthProperty().bind(blockTop.widthProperty());
+        HBox.setHgrow(blockName, Priority.ALWAYS);
+		blockName.setMinWidth(256);
+	    blockName.setMaxWidth(Double.MAX_VALUE);
+		//
+        tableName.setText("tableName nameaaaaaaaaaaaaaaaaaaaaaa!!!!---+aaaaaaaaaaaaaaaaaaaaaaa!");
+        tableName.setStyle("-fx-background-color: pink");
+		tableName.prefHeightProperty().bind(blockTop.heightProperty());
+		tableName.prefWidthProperty().bind(blockTop.widthProperty());
+		tableName.managedProperty().bind(tableName.visibleProperty());
+		//tableName.setVisible(false);
+		//
+        close.setId("close_button");
+		close.setMinWidth(boxH);
+		close.prefHeightProperty().bind(blockTop.heightProperty());
+		//
+        blockTop.getChildren().addAll(back, blockName, tableName, close);
+        //blockTop.getChildren().addAll(back, blockName, close);
         return blockTop;
+    }
+    private HBox initMainPane()
+    {
+        HBox blockMain = new HBox();
+        blockMain.setStyle("-fx-background-color: purple");
+        VBox.setVgrow(blockMain, Priority.ALWAYS);
+        blockMain.getChildren().addAll(initLeftPane());
+        return blockMain;
+    }
+
+    private VBox initLeftPane()
+    {
+		int boxW = 192;
+        VBox blockLeft = new VBox();
+        blockLeft.setStyle("-fx-background-color: red");
+		blockLeft.setPrefWidth(boxW);
+		listView = initListView();
+		listView.prefHeightProperty().bind(blockLeft.heightProperty());
+        blockLeft.getChildren().addAll(listView);
+        return blockLeft;
     }
     private ListView<String> initListView()
     {
